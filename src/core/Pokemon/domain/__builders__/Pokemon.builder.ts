50f989b26c35974220f39192bd61fa1599c3d917
Endika Orube <endika.orube@biko2.com>
2024-04-19 14:12:41 +0200

    refactor: builder pokemon

---
+++ src/core/Pokemon/domain/__builders__/Pokemon.builder.ts
@@ -0,0 +1,30 @@
+import { Pokemon } from "../Pokemon";
+
+export const pokemonBuilder = (options?: Partial<Pokemon>) => {
+  const defaults: Pokemon = {
+    id: "irrelevantId",
+    name: "irrelevantName",
+    height: 0,
+    weight: 0,
+    types: [],
+    images: {
+      main: "irrelevantMainImage",
+      alt: "irrelevantAltImage",
+    },
+    stats: {
+      hp: 0,
+      attack: 0,
+      defense: 0,
+      specialAttack: 0,
+      specialDefense: 0,
+      speed: 0,
+    },
+    isFavorite: false,
+  };
+
+  return {
+    build() {
+      return Object.assign({}, defaults, options);
+    },
+  };
+};
--- src/ui/views/Details/__tests__/Details.test.tsx
+++ src/ui/views/Details/__tests__/Details.test.tsx
@@ -3,31 +3,17 @@ import { Details } from "../Details";
 import { describe, expect, it } from "vitest";
 import { mockUrlParams, render, serviceMockBuilder } from "@/test/utils";
 import { pokemonService } from "@/core/Pokemon/services/Pokemon.service";
+import { pokemonBuilder } from "@/core/Pokemon/domain/__builders__/Pokemon.builder";
 
 describe("Details", () => {
   it("should display name of the pokemon", async () => {
     mockUrlParams({ id: "25" });
     serviceMockBuilder(pokemonService, "getById")
-      .withValue({
-        id: "25",
-        name: "Pikachu",
-        height: 0.4,
-        weight: 6,
-        types: ["electric"],
-        images: {
-          main: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-          alt: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg",
-        },
-        stats: {
-          hp: 35,
-          attack: 55,
-          defense: 40,
-          specialAttack: 50,
-          specialDefense: 56,
-          speed: 90,
-        },
-        isFavorite: false,
-      })
+      .withValue(
+        pokemonBuilder({
+          name: "Pikachu",
+        }).build()
+      )
       .build();
 
     render(<Details />);
