813720d3efd1a28464060ab6aabf5b8fc373ce59
Endika Orube <endika.orube@biko2.com>
2024-04-19 14:07:57 +0200

    test: details

--- src/ui/views/Details/__tests__/Details.test.tsx
+++ src/ui/views/Details/__tests__/Details.test.tsx
@@ -1,39 +1,34 @@
 import { screen } from "@testing-library/react";
 import { Details } from "../Details";
-import { describe, expect, it, vitest } from "vitest";
+import { describe, expect, it } from "vitest";
 import { mockUrlParams, render, serviceMockBuilder } from "@/test/utils";
-import { pokemonServiceContainer } from "@/core/Pokemon/services/_di";
 import { pokemonService } from "@/core/Pokemon/services/Pokemon.service";
 
 describe("Details", () => {
   it("should display name of the pokemon", async () => {
     mockUrlParams({ id: "25" });
-
-    // serviceMockBuilder(
-    //   pokemonServiceContainer,
-    //   ""
-    // )
-    //   .withValue({
-    //     id: "1",
-    //     name: "bulbasaur",
-    //     height: 0.7,
-    //     weight: 6.9,
-    //     types: ["grass", "poison"],
-    //     images: {
-    //       main: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    //       alt: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg",
-    //     },
-    //     stats: {
-    //       hp: 45,
-    //       attack: 49,
-    //       defense: 49,
-    //       specialAttack: 65,
-    //       specialDefense: 65,
-    //       speed: 45,
-    //     },
-    //     isFavorite: false,
-    //   })
-    //   .build();
+    serviceMockBuilder(pokemonService, "getById")
+      .withValue({
+        id: "25",
+        name: "Pikachu",
+        height: 0.4,
+        weight: 6,
+        types: ["electric"],
+        images: {
+          main: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+          alt: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg",
+        },
+        stats: {
+          hp: 35,
+          attack: 55,
+          defense: 40,
+          specialAttack: 50,
+          specialDefense: 56,
+          speed: 90,
+        },
+        isFavorite: false,
+      })
+      .build();
 
     render(<Details />);
 
