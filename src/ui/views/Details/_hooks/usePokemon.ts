de11aa87545309fd62a5f1a33596a98de686555d
Endika Orube <endika.orube@biko2.com>
2024-04-19 14:15:21 +0200

    refactor: elimina container

--- src/ui/views/Details/_hooks/usePokemon.ts
+++ src/ui/views/Details/_hooks/usePokemon.ts
@@ -1,12 +1,12 @@
 import { Pokemon } from "@/core/Pokemon/domain/Pokemon";
-import { pokemonServiceContainer } from "@/core/Pokemon/services/_di";
+import { pokemonService } from "@/core/Pokemon/services/Pokemon.service";
 import { useQueryService } from "@/ui/hooks/useQueryService";
 
 export const usePokemon = (id: Pokemon["id"]) => {
   const { data, hasError, mutate } = useQueryService(
     "pokemon.getById",
     [],
-    () => pokemonServiceContainer("getById")(id)
+    () => pokemonService.getById(id)
   );
 
   return { pokemon: data, hasError, mutate };
