8022329d9737d139ecdf482a704b2d5b73483e65
Endika Orube <endika.orube@biko2.com>
2024-04-22 10:13:08 +0200

    refactor: tipado

--- src/core/_clients/apiClient.ts
+++ src/core/_clients/apiClient.ts
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 export const apiClient = {
   get: async <T>(endpoint: string, options?: { params?: any }) => {
-    const { data } = await axios.get(endpoint, options)
-    return data as T
+    const { data } = await axios.get<T>(endpoint, options)
+
+    return data
   },
 }
