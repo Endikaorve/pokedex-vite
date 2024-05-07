1b37ee3a0b2d39ab746d8329710e85be7d7d0703
Endika Orube <endika.orube@biko2.com>
2024-05-07 08:45:44 +0200

    chore: mejora tipado

--- src/ui/hooks/useQueryService.ts
+++ src/ui/hooks/useQueryService.ts
@@ -9,12 +9,24 @@ import useSWR from 'swr'
 //   disableCache?: boolean;
 // }
 
+interface QueryService<Data> {
+  data: Data | undefined
+  isValidating: boolean
+  hasError: boolean
+  mutate: (
+    data: Data | undefined,
+    options?: {
+      revalidate: boolean
+    }
+  ) => Promise<Data | undefined>
+}
+
 export const useQueryService = <Data>(
   key: string,
   deps: unknown[],
   service: (() => Promise<Data>) | (() => Data)
   // config?: QueryOptions,
-) => {
+): QueryService<Data> => {
   // Usa solo los valores definidos para las dependencias. Así, mismos usos
   // con paso de parámetros distinto funcionan igual. P.e. un opcional
   // "(x?: Type) => Promise" que en un caso no se le pasa parámetro "()" y en
