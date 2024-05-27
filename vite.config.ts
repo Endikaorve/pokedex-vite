293ff5d4c7356becdb539d11f0bb66054733c7c1
Endika Orube <endika.orube@biko2.com>
2024-05-27 12:31:45 +0200

    chore: testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,47 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { UserConfig } from 'vitest'
 
-export default defineConfig({
+type TestOption = 'unit' | 'integration'
+
+const getTestConfig = (mode?: TestOption): UserConfig => {
+  const sharedConfig: UserConfig = {
+    globals: true,
+    environment: 'jsdom',
+  }
+
+  if (mode === 'unit') {
+    return {
+      ...sharedConfig,
+      include: ['**/*.test.ts'],
+    }
+  }
+
+  const integrationConfig: UserConfig = {
+    setupFiles: './src/test/setup.ts',
+  }
+
+  if (mode === 'integration') {
+    return {
+      ...sharedConfig,
+      ...integrationConfig,
+      include: ['**/*.test.tsx'],
+    }
+  }
+
+  return {
+    ...sharedConfig,
+    ...integrationConfig,
+  }
+}
+
+export default defineConfig(({ mode }) => ({
   resolve: {
     alias: {
       '@': '/src',
     },
   },
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-  },
-})
+  test: getTestConfig(mode as TestOption),
+}))
